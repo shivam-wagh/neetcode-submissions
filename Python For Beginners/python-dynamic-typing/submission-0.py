4b03a3c01adafa40bf209516f9786065c5528126
Shivam Wagh <shivamwagh.dev@gmail.com>
2026-08-29 23:04:38 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 5.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Shivam"
+print(type(variable))
+variable = ["Shivam", "Haiyku"]
+print(type(variable))
