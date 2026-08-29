6d35ed3bdeddbc003de26c16115405ccf3e19489
Shivam Wagh <shivamwagh.dev@gmail.com>
2026-08-29 23:02:29 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 22
+float_type = 22.0
+bool_type = True
+string_type = "Shibu"
+list_type = ["Shibu","Hima","Dndr"]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
